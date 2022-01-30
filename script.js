696ff9e621cd4f2e5548d122dc55d3fa8d62072e
Matthew Nothdurft <nothdurftm17@gmail.com>
2022-01-30 13:45:51 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function addShadow(element){
+    element.classList.add("shadow");
+}
+function removeShadow(element){
+    element.classList.remove("shadow");
+}
+
+function over(element){
+    element.style.backgroundColor="lime";
+}
+function out(element){
+    element.style.backgroundColor="silver";
+}
